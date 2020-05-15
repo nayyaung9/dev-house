624dd5cfd1362d92f17522f138bb3d52afb6a919
nayyaung9 <naygit.developer@gmail.com>
2020-05-16 02:07:07 +0630

    test: add data.js and render them into view

---
+++ src/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'Alex Duval',
+    description: 'Fullstack Dev, Teacher, Freeride skier',
+    url: 'https://www.alexduval.fr/uses',
+    twitter: '@xlanex6',
+    emoji: '🎿',
+    country: '🇫🇷',
+    computer: 'apple',
+    phone: 'iphone',
+    tags: [
+      'Developer',
+      'Full Stack',
+      'Web',
+      'jamSTACK',
+      'Vue',
+      'Nuxt',
+      'JavaScript',
+      'TailwindCSS',
+      'Vuetify',
+    ],
+  },
+];
--- src/views/homeView.js
+++ src/views/homeView.js
@@ -1,9 +1,14 @@
 import { elements } from './base';
+import data from '../data';
 
 export const renderHome = () => {
+  const users = data.map((item, index) => {
+    return `<li>${item.name}</li>`
+  })
   const markup = `
     <div>
       Myanmar Dev-House
+      <ul>${users}</ul>
     </div>
   `;
 
