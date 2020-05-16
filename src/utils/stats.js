893647b27df42a39af0652fcff199d6b94fd2137
nayyaung9 <naygit.developer@gmail.com>
2020-05-16 14:32:11 +0630

    feat: rendering tags and users and design layout

---
+++ src/utils/stats.js
@@ -0,0 +1,24 @@
+import data from '../data';
+
+function merge(prop) {
+  return function (acc, obj) {
+    return [...obj[prop], ...acc];
+  };
+}
+
+function countInstances(acc, tag) {
+  acc[tag] = acc[tag] ? acc[tag] + 1 : 1;
+  return acc;
+}
+
+export const tags = () => {
+  const allTags = data.reduce(merge('tags'), []);
+  const counts = allTags.reduce(countInstances, {});
+  // sort and filter for any tags that only have 1
+  const tags = Object.entries(counts)
+    .sort(([, countA], [, countB]) => countB - countA)
+    // Only show the tag if this topic has 3 or more people in it
+    .filter(([, count]) => count >= 3)
+    .map(([name, count]) => ({ name, count }));
+  return [{ name: 'all', count: data.length }, ...tags];
+}
--- src/views/homeView.js
+++ src/views/homeView.js
@@ -1,14 +1,25 @@
 import { elements } from './base';
 import data from '../data';
+import * as utils from '../utils/stats';
 
 export const renderHome = () => {
   const users = data.map((item, index) => {
-    return `<li>${item.name}</li>`
-  })
+    return `<li>${item.name}</li>`;
+  });
+
+  const tags = utils.tags().map((resultTags) => {
+    return `<a class="ui image label" style="background-color: #b8c1ec; ">${resultTags.name}</a>`;
+  });
+
   const markup = `
-    <div>
+    <div class="ui container">
       Myanmar Dev-House
-      <ul>${users}</ul>
+      <br />
+ 
+      ${tags.join("")}
+
+     
+      <ul>${users.join("")}</ul>
     </div>
   `;
 
